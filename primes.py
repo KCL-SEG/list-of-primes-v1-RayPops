dc3ef6ba066ba75dbc1b738b3758d4efb37b93b7
Rayan Popat <rayan.popat@kcl.ac.uk>
2023-10-11 13:24:13 +0100

    Add function tha calculates prime numbers.

--- primes.py
+++ primes.py
@@ -3,4 +3,12 @@
 
 def primes(number_of_primes):
     list = []
+    num = 2
+    while len(list) < number_of_primes:
+        for i in range(2, num):
+            if num % i == 0:
+                break
+        else:
+            list.append(num)
+        num += 1
     return list
